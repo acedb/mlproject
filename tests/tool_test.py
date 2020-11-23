da02a828fca5b238f893254849116748cbc4e8f2
Alice de Bokay <alicedebokay@googlemail.com>
2020-11-23 13:05:02 +0100

    Test creation

---
+++ tests/tool_test.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+
+# Import from standard library
+import os
+import mlproject
+from math import radians, cos, sin, asin, sqrt
+
+# Import from tools
+from mlproject.tools import haversine
+import pytest
+
+def test_haversine():
+    # Le Wagon location
+    lat1, lon1 = 48.865070, 2.380009
+    # My coordinates from google maps here
+    lat2, lon2 = 48.871604158433065, 2.3696945268085834
+    distance = haversine(lon1, lat1, lon2, lat2)
+    assert distance == 1.0474088693924297
